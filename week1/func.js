b20ff56c0179d0b61bb7015a73f1d519267d7e40
SiuHangChan <schan247@my.centennialcollege.ca>
2023-05-16 21:55:04 +0800

    lab1

---
+++ week1/func.js
@@ -0,0 +1,19 @@
+/*function sum1(a, b) {
+    return a + b;
+    }
+    let result = sum1(1, 2);
+    console.log(result);
+*/
+
+/*const sum1 = (a, b) => {
+    return a + b;
+    }
+    let result = sum1(1, 2);
+    console.log(result);
+*/
+
+const sum1 = (a, b) => a + b;
+let result = sum1(7, 2);
+console.log(result);
+
+
